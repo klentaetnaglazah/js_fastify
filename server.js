e0d5bf09bad99f3b03be7c93e618fad400c376d4
klentaetnaglazah <felixxgg42@gmail.com>
2026-04-16 19:50:05 +0400

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
 const fastify = require('fastify')({ logger: true })
 const path = require('path')
 
-fastify.register(require('@fastify/static'), {
-  root: path.join(__dirname, 'src'),
-  prefix: '/',
+fastify.register(require('@fastify/view'), {
+  engine: {
+    pug: require('pug')
+  },
+  root: path.join(__dirname, 'views'),
 })
 
-fastify.get('/api', async (request, reply) => {
-  return 'Успешно'
+fastify.addContentTypeParser(
+  'application/x-www-form-urlencoded',
+  { parseAs: 'string' },
+  function (req, body, done) {
+    try {
+      const parsed = require('querystring').parse(body)
+      done(null, parsed)
+    } catch (err) {
+      done(err)
+    }
+  }
+)
+
+const users = [
+  { id: 1, name: 'Иван Иванов', email: 'vanya@mail.ru' },
+  { id: 2, name: 'Анастасия Петрова', email: 'nastya@mail.ru' },
+  { id: 3, name: 'Дмитрий Сидоров', email: 'dima@mail.ru' }
+]
+
+fastify.get('/', async (req, rep) => {
+  return rep.redirect('/users')
+})
+
+fastify.get('/users', async (req, rep) => {
+  return rep.view('users.pug', { users: users })
+})
+
+fastify.get('/users/create', async (req, rep) => {
+  return rep.view('form.pug')
+})
+
+fastify.post('/users', async (req, rep) => {
+  const { name, email } = req.body
+  const newUser = {
+    id: users.length + 1,
+    name: name,
+    email: email
+  }
+
+  users.push(newUser)
+  return rep.redirect('/users')
 })
 
 const start = async () => {
